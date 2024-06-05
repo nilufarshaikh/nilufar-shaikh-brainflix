6b29536e995701e057905144391a6a5e2c958f34
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-06-05 19:27:22 -0400

    Made a video description component and added styles to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import VideoDescription from "./components/VideoDescription/VideoDescription";
 
 function App() {
   return (
     <>
       <Header />
+      <VideoDescription />
     </>
   );
 }
