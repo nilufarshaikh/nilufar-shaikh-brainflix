ce5ab95e985e74ecd1d2ef58d1bccd2f9f6bbd2c
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-06-11 22:52:25 -0400

    Applied feedbacks received from sprint 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import "./App.scss";
-import { useState } from "react";
-import videoDetails from "./data/video-details.json";
 import Header from "./components/Header/Header";
 import MainContentArea from "./components/MainContentArea/MainContentArea";
 
 function App() {
-  const [videos, setVideos] = useState(videoDetails);
-  const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
-
-  const handleSelectVideo = (clickedId) => {
-    const foundVideo = videoDetails.find((video) => clickedId === video.id);
-    setSelectedVideo(foundVideo);
-  };
-
-  const filteredVideos = videos.filter(
-    (video) => video.id !== selectedVideo.id
-  );
-
   return (
     <>
       <Header />
-      <MainContentArea
-        selectedVideo={selectedVideo}
-        videos={filteredVideos}
-        handleSelectVideo={handleSelectVideo}
-      />
+      <MainContentArea />
     </>
   );
 }
--- src/components/MainContentArea/MainContentArea.jsx
+++ src/components/MainContentArea/MainContentArea.jsx
@@ -1,14 +1,28 @@
 import "./MainContentArea.scss";
+import { useState } from "react";
+import videoDetails from "../../data/video-details.json";
 import VideoPlayer from "../VideoPlayer/VideoPlayer";
 import VideoInfo from "../VideoInfo/VideoInfo";
 
-const MainContentArea = ({ selectedVideo, videos, handleSelectVideo }) => {
+const MainContentArea = () => {
+  const [videos, setVideos] = useState(videoDetails);
+  const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
+
+  const handleSelectVideo = (clickedId) => {
+    const foundVideo = videoDetails.find((video) => clickedId === video.id);
+    setSelectedVideo(foundVideo);
+  };
+
+  const filteredVideos = videos.filter(
+    (video) => video.id !== selectedVideo.id
+  );
+
   return (
     <main className="main">
       <VideoPlayer selectedVideo={selectedVideo} />
       <VideoInfo
         selectedVideo={selectedVideo}
-        videos={videos}
+        videos={filteredVideos}
         handleSelectVideo={handleSelectVideo}
       />
     </main>
